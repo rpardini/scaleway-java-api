ef5ce3d17164754c4bd30d98b5e451256f8a35ff
ricardop <ricardo.pardini@me.com.br>
2018-08-10 14:10:51 +0200

    hack, add boot_type fixed to "bootscript" to maintain old behavior

--- src/main/java/synapticloop/scaleway/api/model/Server.java
+++ src/main/java/synapticloop/scaleway/api/model/Server.java
@@ -39,6 +39,7 @@ public class Server extends ServerBase {
 	@JsonProperty("ipv6")               private IPv6 ipv6;
 	@JsonProperty("image")              private Image image;
 	@JsonProperty("hostname")           private String hostname;
+	@JsonProperty("boot_type")          private String boot_type = "bootscript";
 
 	public String getHostname() { return hostname; }
 
